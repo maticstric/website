e5ba98d850ad77f8c9bbdd7d4a2ecabf70b85e23
maticstric <matic@likar.me>
2026-09-22 16:40:59 -0700

    add site and base into config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://maticstric.github.io',
+  base: '/website', // must match your repo name exactly, case-sensitive
   vite: {
     plugins: [tailwindcss()]
   }
