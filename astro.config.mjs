39a0311ee8297d5ee58c3ef5f62667ffeb8f88ba
maticstric <matic@likar.me>
2026-09-22 16:47:04 -0700

    add CNAME and edit config with custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://maticstric.github.io',
-  base: '/website', // must match your repo name exactly, case-sensitive
+  site: 'https://matic.likar.me',
   vite: {
     plugins: [tailwindcss()]
   }
